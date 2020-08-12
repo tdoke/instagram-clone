b143b1b8948d7aa0a0de9a5032cf8ec388fba14e
Trushant Doke <trushant.doke@xebia.com>
2020-08-13 01:42:53 +0530

    add links

--- src/routes/my-profile/MenuLinks.tsx
+++ src/routes/my-profile/MenuLinks.tsx
@@ -1,20 +1,48 @@
 import * as React from "react";
 import { history } from "routes/history";
+import styled from "styled-components";
+
+const StyledOL = styled.ol`
+  list-style: none;
+
+  li {
+    a {
+      display: block;
+      text-decoration: none;
+      padding: 16px 16px;
+      color: ${props => props.theme.color.grey.dark};
+
+      &:link,
+      &:visited {
+        background-color: #fff;
+        transition: background-color 0.2s;
+      }
+      &:hover,
+      &:active
+      {
+        background-color: ${props => props.theme.color.grey.lightest};
+        border-left: 4px solid ${props => props.theme.color.grey.light};
+      }
+    }
+  }
+`;
 
 export interface MenuLinksProps {}
 
 const MenuLinks: React.SFC<MenuLinksProps> = () => {
   return (
-    <ol>
+    <StyledOL>
       <li>
         <a href="#" onClick={() => history.push("/user/profile/edit/details")}>
           Edit Profile
         </a>
       </li>
       <li>
-        <a href="#" onClick={() => history.push("/user/profile/edit/pwd")}>Change Password</a>
+        <a href="#" onClick={() => history.push("/user/profile/edit/pwd")}>
+          Change Password
+        </a>
       </li>
-    </ol>
+    </StyledOL>
   );
 };
 
